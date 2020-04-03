4325442c5e0fb4c1ef76dcd96601230cf736c2f5
Carlos <carloshenrique@ioasys.com.br>
2020-04-03 13:34:43 -0300

    consertando arquivo de configuração que estava com DB_PASS duplicado

--- config/env/development.js
+++ config/env/development.js
@@ -61,7 +61,7 @@ module.exports = {
       port: process.env.DATABASE_PORT,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASS,
-      database: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_DB,
       ssl: {
         rejectUnauthorized: false
       }
