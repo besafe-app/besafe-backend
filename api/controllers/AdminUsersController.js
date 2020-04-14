b04e3ef265f2a6d425ec512c4871804ddf312e29
Carlos <carloshenrique@ioasys.com.br>
2020-04-14 17:57:37 -0300

    mudanças

--- api/controllers/AdminUsersController.js
+++ api/controllers/AdminUsersController.js
@@ -7,61 +7,50 @@
 
 module.exports = {
   create: async (req, res) => {
-    try {
-      const {
-        email,
-        password,
-        phone,
-        name,
-        gender,
-        birthdate,
-      } = req.allParams();
-      if (email && password) {
-        const targetUser = await AdminUsers.findOne({ email: email });
-        if (!targetUser) {
-          try {
-            const encriptedPass = CryptoService.encrypt(password);
-            const user = await AdminUsers.create({
-              email,
-              password: encriptedPass,
-              phone,
-              name,
-              gender,
-              birthdate,
-            }).fetch();
-            const plainUser = { ...user };
-            const token = JwtService.issue(plainUser);
-            await AdminUsers.updateOne({ id: user.id }).set({ token: token });
-            delete plainUser.password;
-            plainUser.token = token;
-            return res.status(201).json(plainUser);
-          } catch (error) {
-            console.log(error);
-            const status = error.code || error.status || 500;
-            return res.status(status).json(error);
-          }
+    const { email, password, phone, name, gender, birthdate } = req.allParams();
+    if (email && password) {
+      const targetUser = await AdminUsers.findOne({ email: email });
+      if (!targetUser) {
+        try {
+          const encriptedPass = CryptoService.encrypt(password);
+          const user = await AdminUsers.create({
+            email,
+            password: encriptedPass,
+            phone,
+            name,
+            gender,
+            birthdate,
+          }).fetch();
+          const plainUser = { ...user };
+          const token = JwtService.issue(plainUser);
+          await AdminUsers.updateOne({ id: user.id }).set({ token: token });
+          delete plainUser.password;
+          plainUser.token = token;
+          return res.status(201).json(plainUser);
+        } catch (error) {
+          console.log(error);
+          const status = error.code || error.status || 500;
+          return res.status(status).json(error);
         }
-        return res.status(200).json({ message: 'User already registered' });
       }
-      return res.status(400).json({ message: 'Missing arguments' });
-    } catch (error) {
-      return res.status(500).json(error);
+      return res.status(200).json({ message: 'User already registered' });
     }
+    return res.status(400).json({ message: 'Missing arguments' });
   },
   auth: async (req, res) => {
     try {
       const { email, password } = req.allParams();
       const user = await AdminUsers.findOne({ email: email });
       if (user) {
-        const descriptedPass = CryptoService.decrypt(user.password);
-        if (password === descriptedPass) {
+        const decryptedPass = CryptoService.decrypt(user.password);
+        if (password === decryptedPass) {
           const plainUser = { ...user };
           delete plainUser.password;
           return res.status(200).json(plainUser);
         }
-        return res.status(404).json({ message: 'User or password is invalid' });
+        return res.status(422).json({ message: 'User or password is invalid' });
       }
-      return res.status(404).json({ message: 'User or password is invalid' });
+      return res.status(422).json({ message: 'User or password is invalid' });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
--- api/services/CryptoService.js
+++ api/services/CryptoService.js
@@ -1,6 +1,6 @@
 const CriptoJs = require('crypto-js');
-const passphrase = sails.config.secrets.passphrase;
-const frontendPassphrase = sails.config.secrets.frontendPassphrase;
+const passphrase = process.env.PASSPHRASE;
+const frontendPassphrase = process.env.FRONT_PASSPHRASE;
 
 module.exports = {
   encrypt: (data) => {
